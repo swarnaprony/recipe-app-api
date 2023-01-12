32b9dbecb5fa8ad3f6b94dbcfd553136d95f047f
swarnaprony <swarnaprony81@gmail.com>
2023-01-12 02:08:59 +0100

    Connect databases and add unit command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Django Command to wait for the database to be available.
+"""
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """ Django Command to wait for database."""
+
+    def handle(self, *args, **options):
+        pass
