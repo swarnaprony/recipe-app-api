02b35d7d0f38c95094a50cd5194415f70bb59428
swarnaprony <swarnaprony81@gmail.com>
2023-01-18 13:45:34 +0100

    Update tet commands

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,6 +11,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """ Test Commands. """
