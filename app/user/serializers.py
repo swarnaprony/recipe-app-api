15739226f686fe9a29f3bc3eeffa1e1a4513fd5c
swarnaprony <swarnaprony81@gmail.com>
2023-01-24 00:25:09 +0100

    Add user token api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -12,4 +12,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {'password': {}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Create and return a user with encrypte password."""
+        return get_user_model().objects.create_user(**validated_data)
